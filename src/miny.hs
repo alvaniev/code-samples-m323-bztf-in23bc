e195890f31359ee2a0537feb81dd7f9ee8e902be
Andreas Ernst <andreas.ernst@alvaniev.net>
2025-11-26 17:45:27 +0100

    - miny added

---
+++ src/miny.hs
@@ -0,0 +1,10 @@
+import Vector
+import Sampldata
+
+-- foldl iterates over al list of Vectors
+-- accumulator is initialized with the y-value of the first Vector in the list
+-- for each Vector in the list, the lambda function compares its y-value with the current minimum y-value stored in the accumulator
+-- after processing all Vectors, the accumulator holds the minimum y-value found in the list
+minY :: [Vector] -> Double
+minY [] = error "Liste muss mindestens ein Vector haben"
+minY ((Vector _ y) : vecs) = foldl (\acc (Vector _ y) -> min y acc) y vecs
